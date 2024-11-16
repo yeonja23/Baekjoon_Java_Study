94082a9215400b3984040a18e95049267392316d
bomikim <rlaqhals05@naver.com>
2024-11-16 21:39:23 +0900

    11/16 과제

---
+++ bomin/src/Bronze/B11654.java
@@ -0,0 +1,14 @@
+package Bronze;
+import java.io.IOException;
+public class B11654 {
+    public static void main(String[] args)throws IOException{
+        int n = System.in.read();
+        System.out.print(n);
+
+        /*
+        * Scanner in = new Scanner(System.in);
+        * int ch = in.next().charAt(0);
+        * System.out.print(ch);
+        * */
+    }
+}
