0a802de52cb51bc2dacb3225692bfae475e97bdc
Park, Yeonji <yeonpyj23@gmail.com>
2025-09-18 16:07:43 +0900

    9/18

---
+++ src/step4/B5597.java
@@ -0,0 +1,26 @@
+package step4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B5597 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean[] flag = new boolean[30];
+        for (int i = 0; i < 30; i++) {
+            flag[i] = false;
+        }
+
+        for (int i = 0; i < 28; i++) {
+            int num = Integer.parseInt(br.readLine());
+            flag[num - 1] = true;
+        }
+
+        for (int i = 0; i < 30; i++) {
+            if (flag[i] == false) {
+                System.out.println(i + 1);
+            }
+        }
+    }
+}
