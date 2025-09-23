fedab32c9d66493e5ee92fb5cb93c016c5b9c21e
Park, Yeonji <yeonpyj23@gmail.com>
2025-09-23 19:10:11 +0900

    9/23

---
+++ src/step5/B27866.java
@@ -0,0 +1,15 @@
+package step5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B27866 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int i = Integer.parseInt(br.readLine()) - 1;
+
+        System.out.println(str.charAt(i));
+    }
+}
---
+++ src/step5/B9086.java
@@ -0,0 +1,19 @@
+package step5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B9086 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            System.out.println(str.charAt(0) + "" + str.charAt(str.length() - 1));
+        }
+
+    }
+}
