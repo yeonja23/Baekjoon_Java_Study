cc374cd2c8a609fd305aa786359f0c06e20b6c4b
Yeonji Park <163745971+yeonja23@users.noreply.github.com>
2026-03-23 17:23:23 +0900

    [level 2] Title: 가장 큰 수, Time: 3167.40 ms, Memory: 411 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        int size = numbers.length;
+
+        ArrayList<String> str = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            str.add(numbers[i] + "");
+        }
+
+        Collections.sort(str, (a, b) -> (b + a).compareTo(a + b));
+
+        for (String ans : str) {
+            answer += ans;
+        }
+        
+        if (answer.equals("0".repeat(size))) {
+            answer = "0";
+        }
+
+        return answer;
+    }
+}
