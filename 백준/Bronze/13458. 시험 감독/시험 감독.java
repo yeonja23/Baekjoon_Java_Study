0e602efa0c73bb6bcad1e45fc91d90d682e865ab
Yeonji Park <163745971+yeonja23@users.noreply.github.com>
2026-03-29 13:26:41 +0900

    [Bronze II] Title: 시험 감독, Time: 424 ms, Memory: 92276 KB -BaekjoonHub

---
+++ 백준/Bronze/13458. 시험 감독/시험 감독.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        long count = N;
+        for (int num : A) {
+            num -= B;
+            if (num > 0) {
+                count += num / C;
+                if (num % C != 0) {
+                    count++;
+                }
+            }
+        }
+
+        sb.append(count);
+        System.out.println(sb);
+    }
+}
