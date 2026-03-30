eef34de34667f7a1f41e494469d2430e9173b826
Yeonji Park <163745971+yeonja23@users.noreply.github.com>
2026-03-30 22:52:52 +0900

    [Silver I] Title: 절댓값 힙, Time: 316 ms, Memory: 26496 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pQ = new PriorityQueue<>((a, b) -> {
+            if (Math.abs(a) != Math.abs(b)) return Math.abs(a) - Math.abs(b);
+            return a - b;
+        });
+        int x;
+        for (int i = 0; i < N; i++) {
+            x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (pQ.isEmpty()) {
+                    sb.append(0 + "\n");
+                    continue;
+                }
+                sb.append(pQ.poll() + "\n");
+            } else {
+                pQ.add(x);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
